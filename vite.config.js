20179f382974d7173d76cec54c8ada4e00ac7240
Vikram Kumar <vikram.choudhary@recqarz.com>
2025-09-16 18:11:26 +0530

    Added the payment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 4004,
+    host: "0.0.0.0",
+    allowedHosts: true, // Allow all hosts
+    cors: true,
+    hmr: {
+      overlay: false, // 🔴 Disable error overlay (helps you continue working)
+    },
+  },
+});
